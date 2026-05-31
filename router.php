c0bcdc00e127e03e13818980bd17826eb7c91c34
aizor95 <156127916+aizor95@users.noreply.github.com>
2026-05-31 21:17:08 +0200

    protection data

---
+++ router.php
@@ -0,0 +1,21 @@
+<?php
+$uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$chemin = realpath(__DIR__ . $uri);
+$dossierData = realpath(__DIR__ . "/data");
+
+if (
+    $dossierData !== false &&
+    $chemin !== false &&
+    str_starts_with($chemin, $dossierData)
+) {
+    http_response_code(403);
+    echo "Acces interdit.";
+    exit();
+}
+
+if ($chemin !== false && is_file($chemin)) {
+    return false;
+}
+
+require __DIR__ . "/page-d'accueil.php";
+?>

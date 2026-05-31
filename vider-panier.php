927749b29cb2894edbdc23d8d3a578db024a316e
aizor95 <156127916+aizor95@users.noreply.github.com>
2026-05-31 22:22:32 +0200

    secu
    Redirect to commander.php if request method is not POST.

--- vider-panier.php
+++ vider-panier.php
@@ -1,6 +1,11 @@
 <?php
     session_start();
 
+    if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+        header("Location: commander.php");
+        exit();
+    }
+
     unset($_SESSION["panier"]); // Suppression du panier de la session pour le vider après la validation de la commande
 
     header("Location: commander.php");
